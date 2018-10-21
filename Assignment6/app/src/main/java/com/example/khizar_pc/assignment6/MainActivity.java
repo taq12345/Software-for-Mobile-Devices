00ae7e284a9043c053a7cdb013b82af72e9319c2
Talha <talhaahmadqureshi@gmail.com>
2018-10-21 22:50:51 +0500

    assn 6

---
+++ Assignment6/app/src/main/java/com/example/khizar_pc/assignment6/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.khizar_pc.assignment6;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txtview;
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+    private String [] mColumnProjection = new String[]{
+            ContactsContract.Contacts.DISPLAY_NAME_PRIMARY,
+            ContactsContract.Contacts.CONTACT_STATUS,
+            ContactsContract.Contacts.HAS_PHONE_NUMBER
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+        txtview = (TextView) findViewById(R.id.count);
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor=contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
+                mColumnProjection,
+                null,
+                null,
+                null);
+        if (cursor!= null && cursor.getCount()> 0)
+        {
+            StringBuilder sb = new StringBuilder("");
+            while (cursor.moveToNext()){
+                sb.append(cursor.getString(0) + "\n");
+
+            }
+            sb.append("\n" + "Count = " + cursor.getCount());
+            txtview.setText(sb.toString());
+            //Toast.makeText(this, sb.toString(), Toast.LENGTH_LONG);
+
+        }
+        else
+        {
+
+        }
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+}
